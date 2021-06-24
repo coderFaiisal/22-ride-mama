c9661cd325d90443d58d8afe7e0c041c882b8488
ARFaisalweb <officialfaisal1112@gmail.com>
2021-06-24 17:49:38 +0600

    bug fixed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 My sites name: Ride mama
 features: Bus, Train, Bike and Car ride sharing functionality.
+Live Link: https://ride-mama1.web.app/
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBXJArXZR7hki9keZabUnRrx2oUvg_3sCI",
-    authDomain: "ride-mama.firebaseapp.com",
-    projectId: "ride-mama",
-    storageBucket: "ride-mama.appspot.com",
-    messagingSenderId: "636242579255",
-    appId: "1:636242579255:web:2f09ed72729613626d94b2"
-  };
+  apiKey: "AIzaSyDhB1N8BN2kbQ94wlxAKzBN9K3hMdB_6n0",
+  authDomain: "ride-mama1.firebaseapp.com",
+  projectId: "ride-mama1",
+  storageBucket: "ride-mama1.appspot.com",
+  messagingSenderId: "928239071516",
+  appId: "1:928239071516:web:e7c29992ff25e42d75cad9"
+};
   
   export default firebaseConfig;
